996533c3b415e09f38804cefa20b2f8883cc8737
Mateo Rey Nores <mateoreynores@hotmail.com>
2021-10-20 13:28:48 -0300

    default arreglado con distribuciones correctas, implem dist normal

--- TP4/probandoTp4/probandoTp4/TP4.cs
+++ TP4/probandoTp4/probandoTp4/TP4.cs
@@ -15,7 +15,7 @@ namespace probandoTp4
         enum tipo_distribucion { Uniforme, Normal, Exponencial };
         int distribucion_seleccionadaA1, distribucion_seleccionadaA2, distribucion_seleccionadaA3, distribucion_seleccionadaA4, distribucion_seleccionadaA5;
         double[] vecActual, vecAnterior;
-        double minA1, minA2, minA3, minA4, minA5, maxA1, maxA2, maxA3, maxA4, maxA5, semilla, cteA, cteC, cteM;
+        double minA1, minA2, minA3, mediaA3, minA4, minA5, maxA1, maxA2, maxA3, maxA4, maxA5, mediaA5, semilla, cteA, cteC, cteM;
         bool primerSimulacion = true;
         DataTable dt = new DataTable();
         Random r = new Random();
@@ -182,6 +182,7 @@ namespace probandoTp4
                 minA1 = 20;
                 minA2 = 30;
                 minA3 = 40;
+                mediaA3 = 30;
                 minA4 = 10;
                 minA5 = 4;
                 maxA1 = 30;
@@ -189,6 +190,7 @@ namespace probandoTp4
                 maxA3 = 0.115;
                 maxA4 = 20;
                 maxA5 = 5;
+                mediaA5 = 5;
                 distribucion_seleccionadaA1 = 0;
                 distribucion_seleccionadaA2 = 0;
                 distribucion_seleccionadaA3 = 2;
@@ -288,11 +290,13 @@ namespace probandoTp4
                 
             }
             v[0] = x;
-            v[1] = Distribucion.Distribuciones.generarUniforme(minA1, maxA1, v[1]);
+            v[1] = Distribucion.Distribuciones.generarUniforme(minA1,maxA1, v[1]);
             v[2] = Distribucion.Distribuciones.generarUniforme(minA2, maxA2, v[2]);
-            v[3] = Distribucion.Distribuciones.generarUniforme(minA3, maxA3, v[3]);
+            v[3] = Distribucion.Distribuciones.generarExponencial(mediaA3, v[3]);
             v[4] = Distribucion.Distribuciones.generarUniforme(minA4, maxA4, v[4]);
-            v[5] = Distribucion.Distribuciones.generarUniforme(minA5, maxA5, v[5]);
+            v[5] = Distribucion.Distribuciones.generarExponencial(mediaA5, v[5]);
+
+            
 
             return v;
         }
